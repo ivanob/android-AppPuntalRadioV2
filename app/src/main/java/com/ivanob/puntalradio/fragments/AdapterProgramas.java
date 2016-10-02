2d944c893d1086abed63987effb20bbe198d64ba
Ivan Obeso Agüera <ivan@MacBook-Air-de-Ivan.local>
2016-10-02 11:42:05 +0100

    first version working of the programas fragment

--- app/src/main/java/com/ivanob/puntalradio/fragments/AdapterProgramas.java
+++ app/src/main/java/com/ivanob/puntalradio/fragments/AdapterProgramas.java
@@ -5,10 +5,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ivanob.puntalradio.R;
+import com.ivanob.puntalradio.helper.ExpandAndCollapseUtils;
 import com.ivanob.puntalradio.model.RadioProgrammingManager;
 
 /**
@@ -16,41 +20,72 @@ import com.ivanob.puntalradio.model.RadioProgrammingManager;
  */
 public class AdapterProgramas extends RecyclerView.Adapter<AdapterProgramas.ProgramViewHolder> {
     private RadioProgrammingManager progManager;
+    private int expandedPosition = -1;
 
     public static class ProgramViewHolder extends RecyclerView.ViewHolder {
         CardView cv;
         ImageView thumbnail;
         TextView name;
         TextView description;
+        View details;
+        private ViewGroup linearLayoutDetails;
+        private static final int DURATION = 250;
+        private ImageView imageViewExpand;
 
         ProgramViewHolder(View itemView) {
             super(itemView);
-            cv = (CardView)itemView.findViewById(R.id.card_view_program);
-            name = (TextView)itemView.findViewById(R.id.program_name);
-            description = (TextView)itemView.findViewById(R.id.program_description);
-            thumbnail = (ImageView)itemView.findViewById(R.id.thumbnail_program);
+            cv = (CardView) itemView.findViewById(R.id.card_view_program);
+            name = (TextView) itemView.findViewById(R.id.program_name);
+            description = (TextView) itemView.findViewById(R.id.program_description);
+            thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail_program);
+            details = itemView.findViewById(R.id.details_section);
+            linearLayoutDetails = (ViewGroup) itemView.findViewById(R.id.linearLayoutDetails);
+            imageViewExpand = (ImageView) itemView.findViewById(R.id.imageViewExpand);
+            details.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (linearLayoutDetails.getVisibility() == View.GONE) {
+                        ExpandAndCollapseUtils.expand(linearLayoutDetails, DURATION);
+                        imageViewExpand.setImageResource(R.mipmap.more);
+                        rotate(-180.0f);
+                    } else {
+                        ExpandAndCollapseUtils.collapse(linearLayoutDetails, DURATION);
+                        imageViewExpand.setImageResource(R.mipmap.less);
+                        rotate(180.0f);
+                    }
+                }
+            });
         }
+
+            private void rotate(float angle) {
+                Animation animation = new RotateAnimation(0.0f, angle, Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                animation.setFillAfter(true);
+                animation.setDuration(DURATION);
+                imageViewExpand.startAnimation(animation);
+            }
+
     }
+
 
     public AdapterProgramas(RadioProgrammingManager pm) {
         progManager = pm;
     }
 
-    @Override
+
     public ProgramViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.drawable.card_programa, viewGroup, false);
         ProgramViewHolder pvh = new ProgramViewHolder(v);
         return pvh;
     }
 
-    @Override
+
     public void onBindViewHolder(ProgramViewHolder holder, int position) {
         holder.name.setText(progManager.getProgram(position).getNombre());
         holder.description.setText(progManager.getProgram(position).getDescripcion());
         holder.thumbnail.setImageResource(progManager.getProgram(position).getIdLogo());
     }
 
-    @Override
+
     public int getItemCount() {
         return progManager.getNumPrograms();
     }
--- app/src/main/java/com/ivanob/puntalradio/fragments/ProgramasFragment.java
+++ app/src/main/java/com/ivanob/puntalradio/fragments/ProgramasFragment.java
@@ -7,8 +7,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 import com.ivanob.puntalradio.R;
+import com.ivanob.puntalradio.helper.ExpandAndCollapseUtils;
 import com.ivanob.puntalradio.model.RadioProgrammingManager;
 
 import java.io.IOException;
@@ -20,6 +24,9 @@ public class ProgramasFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private ViewGroup linearLayoutDetails;
+    private static final int DURATION = 250;
+    private ImageView imageViewExpand;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,6 +34,8 @@ public class ProgramasFragment extends Fragment {
     }
 
     public void onActivityCreated(Bundle savedInstanceState) {
+        linearLayoutDetails = (ViewGroup) getActivity().findViewById(R.id.linearLayoutDetails);
+        imageViewExpand = (ImageView) getActivity().findViewById(R.id.imageViewExpand);
         mRecyclerView = (RecyclerView) getActivity().findViewById(R.id.recycler_programas);
 
         // use this setting to improve performance if you know that changes
@@ -47,6 +56,18 @@ public class ProgramasFragment extends Fragment {
         mAdapter = new AdapterProgramas(pm);
         mRecyclerView.setAdapter(mAdapter);
         super.onActivityCreated(savedInstanceState);
+    }
+
+    public void toggleDetails(View view) {
+
+    }
+
+    private void rotate(float angle) {
+        Animation animation = new RotateAnimation(0.0f, angle, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setFillAfter(true);
+        animation.setDuration(DURATION);
+        imageViewExpand.startAnimation(animation);
     }
 
 }
